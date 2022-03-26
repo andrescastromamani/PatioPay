e66253a82f2c23db62e1a42b01bce0df5b8d02b1
Andrés Castro Mamani <castroandresrm@gmail.com>
2022-03-26 11:15:30 -0400

    Update data

--- src/components/ExpandableRows.jsx
+++ src/components/ExpandableRows.jsx
@@ -3,39 +3,39 @@ import React from 'react'
 export const ExpandableRows = ({ data }) => {
     return (
         <div className="container">
-            <div className="row">
+            <div className="row p-2">
                 <div className="col">
-                    <label htmlFor="name">Nombre:</label>
+                    <label htmlFor="name" className="form-label">Nombre:</label>
                     <input type="text" className="form-control" disabled value={data.name} />
                 </div>
                 <div className="col">
-                    <label htmlFor="name">Correo Electronico:</label>
+                    <label htmlFor="name" className="form-label">Correo Electronico:</label>
                     <input type="text" className="form-control" disabled value={data.email} />
                 </div>
                 <div className="col">
-                    <label htmlFor="name">Ciudad:</label>
+                    <label htmlFor="name" className="form-label">Ciudad:</label>
                     <input type="text" className="form-control" disabled value={data.city} />
                 </div>
                 <div className="col">
-                    <label htmlFor="name">Codigo Pin:</label>
+                    <label htmlFor="name" className="form-label">Codigo Pin:</label>
                     <input type="text" className="form-control" disabled value={data.pincode} />
                 </div>
                 <div className="col">
-                    <label htmlFor="name">Prioridad:</label>
+                    <label htmlFor="name" className="form-label">Prioridad:</label>
                     <input type="text" className="form-control" disabled value={data.priority} />
                 </div>
             </div>
-            <div className="row">
+            <div className="row p-2">
                 <div className="col-6">
-                    <label htmlFor="name">Direccion:</label>
+                    <label htmlFor="name" className="form-label">Direccion:</label>
                     <input type="text" className="form-control" disabled value={data.address} />
                 </div>
                 <div className="col">
-                    <label htmlFor="name">Telefono:</label>
+                    <label htmlFor="name" className="form-label">Telefono:</label>
                     <input type="text" className="form-control" disabled value={data.phone} />
                 </div>
                 <div className="col">
-                    <label htmlFor="name">Categoria Comercio:</label>
+                    <label htmlFor="name" className="form-label">Categoria Comercio:</label>
                     <input type="text" className="form-control" disabled value={data.category} />
                 </div>
             </div>
